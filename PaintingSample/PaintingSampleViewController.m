0d8fad0bc1cb0cab685908c168594e2d2b993ca8
Sean Christmann <sean.christmann@effectiveui.com>
2011-10-07 12:16:17 -0600

    Initial Commit

---
+++ PaintingSample/PaintingSampleViewController.m
@@ -0,0 +1,44 @@
+//
+//  PaintingSampleViewController.m
+//  PaintingSample
+//
+//  Created by Sean Christmann on 10/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PaintingSampleViewController.h"
+
+@implementation PaintingSampleViewController
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
