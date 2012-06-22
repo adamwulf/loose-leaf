72fc9bfacbd8ebd01af1dd5c9bb17be6a9d0d766
Adam Wulf <adam.wulf@gmail.com>
2012-06-21 22:25:50 -0500

    done with sharing icon

--- Paper Stack/Paper Stack/Constants.h
+++ Paper Stack/Paper Stack/Constants.h
@@ -19,7 +19,7 @@
 #define kGutterWidthToDragPages 300
 #define kFingerWidth 40
 #define kFilteringFactor 0.2
-#define kWidthOfSidebarButton 60
+#define kWidthOfSidebarButton 40
 #define kWidthOfSidebar 80
 
 CGFloat DistanceBetweenTwoPoints(CGPoint point1,CGPoint point2);
