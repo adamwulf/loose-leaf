0f58854abe4208b4c81d75bca4e030a2cdf84c4f
Adam Wulf <adam.wulf@gmail.com>
2012-06-21 19:04:38 -0500

    text button is coming along. the T is upside down :(

---
+++ Paper Stack/Paper Stack/SLTextButton.h
@@ -0,0 +1,13 @@
+//
+//  SLTextButton.h
+//  Paper Stack
+//
+//  Created by Adam Wulf on 6/21/12.
+//  Copyright (c) 2012 Visere. All rights reserved.
+//
+
+#import "SLSidebarButton.h"
+
+@interface SLTextButton : SLSidebarButton
+
+@end
