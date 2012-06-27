baccf6781bd269d32d46048e299c64c448e00ef6
Adam Wulf <adam.wulf@gmail.com>
2012-06-26 23:47:13 -0500

    revamped icons slightly. the circle was off and the pencil was off kilter

--- Paper Stack/Paper Stack/SLTextButton.m
+++ Paper Stack/Paper Stack/SLTextButton.m
@@ -53,7 +53,7 @@
                                                                (drawingWidth - glyphRect.size.height) / 2 + kWidthOfSidebarButtonBuffer)];
     
     //// Oval Drawing
-    UIBezierPath* ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(CGRectGetMinX(frame) + floor(CGRectGetWidth(frame) * 0.01) + 0.5, CGRectGetMinY(frame) + floor(CGRectGetHeight(frame) * 0.01) + 0.5, floor(CGRectGetWidth(frame) * 0.97), floor(CGRectGetHeight(frame) * 0.97))];
+    UIBezierPath* ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(CGRectGetMinX(frame) + 0.5, CGRectGetMinY(frame) + 0.5, floor(CGRectGetWidth(frame) - 0.5), floor(CGRectGetHeight(frame) - 0.5))];
     [ovalPath appendPath:glyphPath];
     [ovalPath closePath];
     [halfGreyFill setFill];
