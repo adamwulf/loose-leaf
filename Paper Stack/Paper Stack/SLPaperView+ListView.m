ec4cf6a459891c2e7021e05ca88c66b166e6b32b
Adam Wulf <adam.wulf@gmail.com>
2012-09-01 22:56:51 -0500

    more simplification around the paper's list view info

--- Paper Stack/Paper Stack/SLPaperView+ListView.m
+++ Paper Stack/Paper Stack/SLPaperView+ListView.m
@@ -9,50 +9,17 @@
 #import "SLPaperView+ListView.h"
 
 
-@interface SLPaperView (ListView_Private)
-
--(NSInteger) rowInListViewGivenIndex:(NSInteger) indexOfPage;
-
--(NSInteger) columnInListViewGivenIndex:(NSInteger) indexOfPage;
-
-@end
-
 
 @implementation SLPaperView (ListView)
 
--(NSInteger) rowInListViewGivenIndex:(NSInteger) indexOfPage{
-    NSInteger rowOfPage = floor(indexOfPage / kNumberOfColumnsInListView);
-    return rowOfPage;
-}
-
--(NSInteger) columnInListViewGivenIndex:(NSInteger) indexOfPage{
-    NSInteger columnOfPage = indexOfPage % kNumberOfColumnsInListView;
-    return columnOfPage;
-}
-
 -(NSInteger) rowInListView{
     NSInteger indexOfPage = [self.delegate indexOfPageInCompleteStack:self];
-    return [self rowInListViewGivenIndex:indexOfPage];
+    return [self.delegate rowInListViewGivenIndex:indexOfPage];
 }
 
 -(NSInteger) columnInListView{
     NSInteger indexOfPage = [self.delegate indexOfPageInCompleteStack:self];
-    return [self columnInListViewGivenIndex:indexOfPage];
-}
-
--(CGRect) frameForListViewGivenRowHeight:(CGFloat)rowHeight andColumnWidth:(CGFloat)columnWidth{
-    CGFloat bufferWidth = kListPageZoom * columnWidth;
-    
-    NSInteger indexOfPage = [self.delegate indexOfPageInCompleteStack:self];
-    NSInteger column = [self columnInListViewGivenIndex:indexOfPage];
-    NSInteger row = [self rowInListViewGivenIndex:indexOfPage];
-    CGRect newFrame = CGRectZero;
-    newFrame.origin.x = bufferWidth + bufferWidth * column + columnWidth * column;
-    newFrame.origin.y = bufferWidth + bufferWidth * row + rowHeight * row;
-    newFrame.size.width = columnWidth;
-    newFrame.size.height = rowHeight;
-
-    return newFrame;
+    return [self.delegate columnInListViewGivenIndex:indexOfPage];
 }
 
 @end
