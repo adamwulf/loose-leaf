3ceb9288cef2ffd5e24586cba47658a6b61a4f2d
Adam Wulf <adam.wulf@gmail.com>
2012-09-02 21:21:28 -0500

    moving pages works

--- Paper Stack/Paper Stack/UIView+SubviewStacks.h
+++ Paper Stack/Paper Stack/UIView+SubviewStacks.h
@@ -20,5 +20,7 @@
 - (NSArray*) peekSubviewFromSubview:(SLPaperView*)obj;
 - (SLPaperView*) getPageBelow:(SLPaperView*)page;
 -(SLPaperView*) getPageAbove:(SLPaperView*)page;
+-(void) insertPage:(SLPaperView*)pageToInsert belowPage:(SLPaperView*)referencePage;
+-(void) insertPage:(SLPaperView*)pageToInsert abovePage:(SLPaperView*)referencePage;
 
 @end
