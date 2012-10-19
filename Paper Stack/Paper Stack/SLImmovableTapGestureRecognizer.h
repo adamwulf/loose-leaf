4f3e1ddb38c78a8af86a124f581184b29f4e2d11
Adam Wulf <adam.wulf@gmail.com>
2012-10-19 14:39:19 -0500

    added comments explaining why I need custom long press and custom tap gestures

--- Paper Stack/Paper Stack/SLImmovableTapGestureRecognizer.h
+++ Paper Stack/Paper Stack/SLImmovableTapGestureRecognizer.h
@@ -9,6 +9,14 @@
 #import <UIKit/UIKit.h>
 #import <UIKit/UIGestureRecognizerSubclass.h>
 
+
+/**
+ *
+ * For notes on why this class even exists, look at the documentation
+ * for SLObjectSelectLongPressGestureRecognizer. The purpose of both
+ * these classes is essentially the same.
+ *
+ */
 @interface SLImmovableTapGestureRecognizer : UITapGestureRecognizer{
     NSMutableDictionary* touchLocations;
 }
