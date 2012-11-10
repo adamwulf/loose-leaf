5efe83d9ae304347f96c1f43de648d86748f5141
Adam Wulf <adam.wulf@gmail.com>
2012-11-10 15:04:41 -0600

    moved the segment class to its own file

--- Paper Stack/Paper Stack/PaintView.mm
+++ Paper Stack/Paper Stack/PaintView.mm
@@ -9,46 +9,7 @@
 #import "PaintView.h"
 #import "NSThread+BlockAdditions.h"
 #import "SLPaperView.h"
-
-@interface StrokeSegment : NSObject{
-    CGFloat fingerWidth;
-    CGRect rectToDisplay;
-    UIBezierPath* path;
-    BOOL shouldFillInsteadOfStroke;
-}
-@property (nonatomic, assign) CGFloat fingerWidth;
-@property (nonatomic, assign) CGRect rectToDisplay;
-@property (nonatomic, retain) UIBezierPath* path;
-@property (nonatomic, assign) BOOL shouldFillInsteadOfStroke;
-
-@end
-
-@implementation StrokeSegment
-@synthesize rectToDisplay;
-@synthesize fingerWidth;
-@synthesize path;
-@synthesize shouldFillInsteadOfStroke;
-
-+(StrokeSegment*) strokeWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path{
-    return [[[StrokeSegment alloc] initWithFingerWidth:_fingerWidth andRect:_rectToDisplay andPath:_path andFill:NO] autorelease];
-}
-
-+(StrokeSegment*) strokeWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path andFill:(BOOL)_fill{
-    return [[[StrokeSegment alloc] initWithFingerWidth:_fingerWidth andRect:_rectToDisplay andPath:_path andFill:_fill] autorelease];
-}
-
--(id) initWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path andFill:(BOOL)_shouldFillInsteadOfStroke{
-    if(self = [super init]){
-        self.fingerWidth = _fingerWidth;
-        self.rectToDisplay = _rectToDisplay;
-        self.path = _path;
-        self.shouldFillInsteadOfStroke = _shouldFillInsteadOfStroke;
-    }
-    return self;
-}
-
-@end
-
+#import "StrokeSegment.h"
 
 @implementation PaintView
 
---
+++ Paper Stack/Paper Stack/StrokeSegment.m
@@ -0,0 +1,35 @@
+//
+//  StrokeSegment.m
+//  scratchpaper
+//
+//  Created by Adam Wulf on 11/10/12.
+//
+//
+
+#import "StrokeSegment.h"
+
+@implementation StrokeSegment
+@synthesize rectToDisplay;
+@synthesize fingerWidth;
+@synthesize path;
+@synthesize shouldFillInsteadOfStroke;
+
++(StrokeSegment*) strokeWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path{
+    return [[[StrokeSegment alloc] initWithFingerWidth:_fingerWidth andRect:_rectToDisplay andPath:_path andFill:NO] autorelease];
+}
+
++(StrokeSegment*) strokeWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path andFill:(BOOL)_fill{
+    return [[[StrokeSegment alloc] initWithFingerWidth:_fingerWidth andRect:_rectToDisplay andPath:_path andFill:_fill] autorelease];
+}
+
+-(id) initWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path andFill:(BOOL)_shouldFillInsteadOfStroke{
+    if(self = [super init]){
+        self.fingerWidth = _fingerWidth;
+        self.rectToDisplay = _rectToDisplay;
+        self.path = _path;
+        self.shouldFillInsteadOfStroke = _shouldFillInsteadOfStroke;
+    }
+    return self;
+}
+
+@end
