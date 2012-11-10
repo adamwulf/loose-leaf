2e851b9102cc6e5d546e94c2455d78836dc56e49
Adam Wulf <adam.wulf@gmail.com>
2012-11-10 15:04:41 -0600

    moved the segment class to its own file

---
+++ Paper Stack/Paper Stack/StrokeSegment.h
@@ -0,0 +1,27 @@
+//
+//  StrokeSegment.h
+//  scratchpaper
+//
+//  Created by Adam Wulf on 11/10/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StrokeSegment : NSObject{
+    CGFloat fingerWidth;
+    CGRect rectToDisplay;
+    UIBezierPath* path;
+    BOOL shouldFillInsteadOfStroke;
+}
+@property (nonatomic, assign) CGFloat fingerWidth;
+@property (nonatomic, assign) CGRect rectToDisplay;
+@property (nonatomic, retain) UIBezierPath* path;
+@property (nonatomic, assign) BOOL shouldFillInsteadOfStroke;
+
++(StrokeSegment*) strokeWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path;
+
++(StrokeSegment*) strokeWithFingerWidth:(CGFloat)_fingerWidth andRect:(CGRect)_rectToDisplay andPath:(UIBezierPath*)_path andFill:(BOOL)_fill;
+
+
+@end
