80ccf129bbf8dd4220848c3c1fb945a61714f92c
Adam Wulf <adam.wulf@gmail.com>
2013-07-10 15:42:29 -0500

    cleaning up the ruler gesture and removing pan specific code #21

--- Project/LooseLeaf/MMRulerToolGestureRecognizer.h
+++ Project/LooseLeaf/MMRulerToolGestureRecognizer.h
@@ -24,35 +24,12 @@
     NSMutableSet* ignoredTouches;
     NSMutableOrderedSet* validTouches;
 
-    // track which bezels our delegate cares about
-    MMBezelDirection bezelDirectionMask;
-    // the direction that the user actually did exit, if any
-    MMBezelDirection didExitToBezel;
     // track the direction of the scale
     MMBezelScaleDirection scaleDirection;
-
-    //
-    // store panning velocity so we can continue
-    // the animation after the gesture ends
-    NSMutableArray* velocities;
-    CGPoint _averageVelocity;
-
-    //
-    // have they bezeled off the page more than once
-    // during the same gesture? this will track it
-    NSInteger numberOfRepeatingBezels;
-    //
-    // don't allow both the 2nd to last touch
-    // and the last touch to trigger a repeat
-    // of the bezel
-    BOOL secondToLastTouchDidBezel;
 }
 
 @property (nonatomic, readonly) NSInteger numberOfRepeatingBezels;
 @property (nonatomic, readonly) CGFloat scale;
-@property (nonatomic, readonly) CGPoint velocity;
-@property (nonatomic, assign) MMBezelDirection bezelDirectionMask;
-@property (nonatomic, readonly) MMBezelDirection didExitToBezel;
 @property (nonatomic, readonly) MMBezelScaleDirection scaleDirection;
 
 -(void) cancel;
