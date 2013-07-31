b88becb42e8e38bb7524650fd122239fb546475e
Adam Wulf <adam.wulf@gmail.com>
2013-07-31 18:39:29 -0500

    fixed drawing not working when the jot manager was disabled

---
+++ Project/LooseLeaf/UIGestureRecognizer+GestureDebug.h
@@ -0,0 +1,13 @@
+//
+//  UIGestureRecognizer+GestureDebug.h
+//  LooseLeaf
+//
+//  Created by Adam Wulf on 7/31/13.
+//  Copyright (c) 2013 Milestone Made, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIGestureRecognizer (GestureDebug)
+
+@end
