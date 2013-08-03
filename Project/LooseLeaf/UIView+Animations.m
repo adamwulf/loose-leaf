e9ed1988c066fc1b3dcf2f03afce006517b83093
Adam Wulf <adam.wulf@gmail.com>
2013-08-02 21:58:40 -0500

    bezeling from the left bezel is working. i still need to disallow bezeling at the left and right at the same time. #114

--- Project/LooseLeaf/UIView+Animations.m
+++ Project/LooseLeaf/UIView+Animations.m
@@ -13,23 +13,24 @@
 @implementation UIView (Animations)
 
 -(void) removeAllAnimationsAndPreservePresentationFrame{
-    // look at the presentation of the view (as would be seen during animation)
-    CGRect lFrame = [self.layer.presentationLayer frame];
-    // look at the view frame to compare
-    CGRect vFrame = self.frame;
-    if([self isKindOfClass:[MMShadowedView class]]){
-        vFrame = [MMShadowedView expandFrame:vFrame];
+    if([[self.layer animationKeys] count]){
+        // look at the presentation of the view (as would be seen during animation)
+        CGRect lFrame = [self.layer.presentationLayer frame];
+        // look at the view frame to compare
+        CGRect vFrame = self.frame;
+        if([self isKindOfClass:[MMShadowedView class]]){
+            vFrame = [MMShadowedView expandFrame:vFrame];
+        }
+        if(!CGRectEqualToRect(lFrame, vFrame) && !CGRectEqualToRect(lFrame, CGRectZero)){
+            // if they're not equal, then remove all animations
+            // and set the frame to the presentation layer's frame
+            // so that the gesture will pick up in the middle
+            // of the animation instead of immediately reset to
+            // its end state
+            self.frame = lFrame;
+        }
+        [self.layer removeAllAnimations];
     }
-    if(!CGRectEqualToRect(lFrame, vFrame) && !CGRectEqualToRect(lFrame, CGRectZero)){
-        // if they're not equal, then remove all animations
-        // and set the frame to the presentation layer's frame
-        // so that the gesture will pick up in the middle
-        // of the animation instead of immediately reset to
-        // its end state
-        self.frame = lFrame;
-    }
-    [self.layer removeAllAnimations];
-
 }
 
 
