2019719ae7728307d37067674c3e3fb684edbe3f
Adam Wulf <adam.wulf@gmail.com>
2013-08-27 00:01:02 -0500

    working on some options for the sidebar bubble for scraps #149

---
+++ Project/LooseLeaf/MMScrapBubbleView.h
@@ -0,0 +1,13 @@
+//
+//  MMScrapBubbleView.h
+//  LooseLeaf
+//
+//  Created by Adam Wulf on 8/26/13.
+//  Copyright (c) 2013 Milestone Made, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMScrapBubbleView : UIView
+
+@end
