9e598632664158bd5b934f2d767b97e7f689386c
Adam Wulf <adam.wulf@gmail.com>
2013-08-26 22:08:23 -0500

    fixed order that scraps are grabbed #169

---
+++ Project/LooseLeaf/NSArray+Extras.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+Extras.h
+//  LooseLeaf
+//
+//  Created by Adam Wulf on 8/26/13.
+//  Copyright (c) 2013 Milestone Made, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Extras)
+
+-(NSArray*) reverseArray;
+
+@end
