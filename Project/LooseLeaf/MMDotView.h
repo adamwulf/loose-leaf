14c3cb7b74a10501838862d56aac67d29af9a1dc
Adam Wulf <adam.wulf@gmail.com>
2013-08-27 00:30:39 -0500

    working on the blinking dot for the shape drawing dashed line #172

---
+++ Project/LooseLeaf/MMDotView.h
@@ -0,0 +1,13 @@
+//
+//  MMDotView.h
+//  LooseLeaf
+//
+//  Created by Adam Wulf on 8/27/13.
+//  Copyright (c) 2013 Milestone Made, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMDotView : UIView
+
+@end
