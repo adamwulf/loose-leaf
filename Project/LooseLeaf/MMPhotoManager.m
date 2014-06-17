b0f7c844c7e946312f4877bd7d6fcd090d68e63e
Adam Wulf <adam.wulf@gmail.com>
2014-06-17 16:54:54 -0500

    if camera roll is empty, taking a photo will still add it to the list #534

--- Project/LooseLeaf/MMPhotoManager.m
+++ Project/LooseLeaf/MMPhotoManager.m
@@ -201,6 +201,11 @@ NSArray*(^arrayByRemovingObjectWithURL)(NSArray* arr, NSURL* url) = ^NSArray*(NS
             return album;
         }
     }
+//    [[self assetsLibrary] groupForURL:url resultBlock:^(ALAssetsGroup *group) {
+//        <#code#>
+//    } failureBlock:^(NSError *error) {
+//        <#code#>
+//    }];
     return nil;
 }
 
@@ -232,7 +237,7 @@ NSArray*(^arrayByRemovingObjectWithURL)(NSArray* arr, NSURL* url) = ^NSArray*(NS
                                                         }
                                                         hasEverInitailized = YES;
                                                         [self.delegate performSelectorOnMainThread:@selector(doneLoadingPhotoAlbums) withObject:nil waitUntilDone:NO];
-                                                    }else if ([group numberOfAssets] > 0){
+                                                    }else if ([group numberOfAssets] > 0 || group.type == ALAssetsGroupSavedPhotos){
                                                         MMPhotoAlbum* addedAlbum = [self albumWithURL:group.url];
                                                         if(!addedAlbum){
                                                             addedAlbum = [[MMPhotoAlbum alloc] initWithAssetGroup:group];
