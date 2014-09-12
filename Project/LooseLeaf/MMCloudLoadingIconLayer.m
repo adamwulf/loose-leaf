3efca586eb45d2e06ed2def1f79166a075274d2e
Adam Wulf <adam.wulf@gmail.com>
2014-09-12 15:39:04 -0500

    new loading cloud icon. no more animating gradient - will animate rotating arrows intead #759

--- Project/LooseLeaf/MMCloudLoadingIconLayer.m
+++ Project/LooseLeaf/MMCloudLoadingIconLayer.m
@@ -23,18 +23,24 @@
         UIBezierPath* cloudPath = [self cloudPathForRect:self.bounds];
         CAShapeLayer* cloudBorderLayer = [self cloudBorderLayerForPath:cloudPath];
         UIColor* borderColor = [UIColor colorWithRed: 0.221 green: 0.221 blue: 0.219 alpha: 1];
+        UIColor* halfWhite = [UIColor colorWithRed: 1 green: 1 blue: 1 alpha: 0.503];
         cloudBorderLayer.strokeColor = borderColor.CGColor;
         cloudBorderLayer.lineWidth = 1;
         cloudBorderLayer.fillColor = [UIColor clearColor].CGColor;
         
         CAShapeLayer* cloudMaskLayer = [self cloudBorderLayerForPath:cloudPath];
-        
         CAGradientLayer* gradientLayer = [self animatingGradientLayer:cloudPath];
         gradientLayer.mask = cloudMaskLayer;
         
-        
         [self addSublayer:gradientLayer];
         [self addSublayer:cloudBorderLayer];
+        
+        UIBezierPath* arrowsPath = [self arrowPathForRect:self.bounds];
+        CAShapeLayer* animatingArrowsLayer = [self animatingArrowsLayer:arrowsPath];
+        animatingArrowsLayer.strokeColor = borderColor.CGColor;
+        animatingArrowsLayer.lineWidth = 1;
+        animatingArrowsLayer.fillColor = halfWhite.CGColor;
+        [self addSublayer:animatingArrowsLayer];
         
         // setup our location
         self.position = CGPointMake(frame.origin.x + frame.size.width/2, frame.origin.y + frame.size.height/2);
@@ -42,7 +48,44 @@
     return self;
 }
 
-
+-(UIBezierPath*) arrowPathForRect:(CGRect)frame{
+    //// CircleArrows Drawing
+    UIBezierPath* circleArrowsPath = UIBezierPath.bezierPath;
+    [circleArrowsPath moveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.56502 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.24312 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.58499 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.27222 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.57367 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.25177 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.58033 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26166 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.60928 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26089 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.59862 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26586 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.60928 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26089 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.59299 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30565 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.60928 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26089 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.59948 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.28783 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.59314 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31100 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.59309 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30743 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.59314 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30921 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.59104 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31100 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.59314 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31100 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.59236 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31100 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.58739 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32103 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.58886 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31699 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.58739 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32103 * CGRectGetHeight(frame))];
+    [circleArrowsPath addLineToPoint: CGPointMake(CGRectGetMinX(frame) + 0.52725 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.29914 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.55597 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.28575 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.52725 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.29914 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.54029 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.29306 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.54240 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26575 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.55285 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.27848 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.54833 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.27168 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.45189 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.26575 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.51740 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.24075 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.47688 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.24075 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.43516 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.29500 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.44349 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.27414 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.43792 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.28428 * CGRectGetHeight(frame))];
+    [circleArrowsPath addLineToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40248 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.29500 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.42926 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.24312 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.40567 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.27600 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.41460 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.25778 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.56502 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.24312 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.46675 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.20563 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.52753 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.20563 * CGRectGetHeight(frame))];
+    [circleArrowsPath closePath];
+    [circleArrowsPath moveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40689 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30097 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.46703 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32286 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.40689 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30097 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.46703 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32286 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.43831 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.33625 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.46703 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32286 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.45399 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32894 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.45189 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.35625 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.44143 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.34352 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.44595 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.35032 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.54240 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.35625 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.47688 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.38125 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.51740 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.38125 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.55912 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32700 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.55079 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.34786 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.55636 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.33772 * CGRectGetHeight(frame))];
+    [circleArrowsPath addLineToPoint: CGPointMake(CGRectGetMinX(frame) + 0.59181 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.32700 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.56502 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.37888 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.58861 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.34600 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.57968 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.36422 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.42926 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.37888 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.52753 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.41637 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.46675 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.41637 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40929 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.34978 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.42061 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.37023 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.41395 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.36034 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.38500 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.36111 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.39566 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.35614 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.38500 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.36111 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40129 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31635 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.38500 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.36111 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.39480 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.33417 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40114 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31100 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.40119 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31457 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.40114 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31279 * CGRectGetHeight(frame))];
+    [circleArrowsPath addLineToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40324 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.31100 * CGRectGetHeight(frame))];
+    [circleArrowsPath addCurveToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40689 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30097 * CGRectGetHeight(frame)) controlPoint1: CGPointMake(CGRectGetMinX(frame) + 0.40542 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30501 * CGRectGetHeight(frame)) controlPoint2: CGPointMake(CGRectGetMinX(frame) + 0.40689 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30097 * CGRectGetHeight(frame))];
+    [circleArrowsPath addLineToPoint: CGPointMake(CGRectGetMinX(frame) + 0.40689 * CGRectGetWidth(frame), CGRectGetMinY(frame) + 0.30097 * CGRectGetHeight(frame))];
+    [circleArrowsPath closePath];
+    return circleArrowsPath;
+}
 
 -(UIBezierPath*) cloudPathForRect:(CGRect)frame{
     UIBezierPath* cloudPath = UIBezierPath.bezierPath;
@@ -69,6 +112,26 @@
     return cloudBorderLayer;
 }
 
+-(CAShapeLayer*) animatingArrowsLayer:(UIBezierPath*)arrowPath{
+    CAShapeLayer* arrowsLayer = [CAShapeLayer layer];
+    CGRect pathBounds = [arrowPath bounds];
+    arrowsLayer.bounds = pathBounds;
+//    arrowsLayer.anchorPoint = CGPointMake(.5, .5);
+    arrowsLayer.position = CGPointMake(pathBounds.origin.x + pathBounds.size.width/2, pathBounds.origin.y + pathBounds.size.height/2);
+    
+    arrowsLayer.path = arrowPath.CGPath;
+    
+    CABasicAnimation *rotateAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotateAnimation.fromValue = @(0);
+    rotateAnimation.toValue = @(2*M_PI);
+    rotateAnimation.removedOnCompletion = NO;
+    rotateAnimation.duration = 1.5f;
+    rotateAnimation.repeatCount = HUGE_VALF; // repeat forever
+    [arrowsLayer addAnimation:rotateAnimation forKey:@"rotateAnimation"];
+
+    return arrowsLayer;
+}
+
 -(CAGradientLayer*)animatingGradientLayer:(UIBezierPath*)cloudPath{
     CAGradientLayer* cloudGradientLayer = [CAGradientLayer layer];
     cloudGradientLayer.bounds = [cloudPath bounds];
@@ -76,51 +139,17 @@
     cloudGradientLayer.position = [cloudPath bounds].origin;
 
     //// Gradient Declarations
-    CGFloat maxAlpha = .5;
-    CGFloat minAlpha = .2;
+    CGFloat maxAlpha = .67;
+    CGFloat minAlpha = .22;
     NSArray* allColors = @[];
     CGFloat maxColors = 10;
     UIColor* originalWhite = [UIColor colorWithRed:1 green:1 blue:1 alpha:1];
-    for (int i=0;i<maxColors*2;i++) {
-        CGFloat angle = sinf(i / (maxColors*2) * M_PI);
+    for (int i=0;i<maxColors;i++) {
+        CGFloat angle = cosf(i / (maxColors*2) * M_PI);
         allColors = [allColors arrayByAddingObject:(id)[originalWhite colorWithAlphaComponent:minAlpha + angle*(maxAlpha - minAlpha)].CGColor];
     }
     
-    NSArray* steppedColors = @[];
-    for(int i=0;i<maxColors*2;i++){
-        steppedColors = [steppedColors arrayByAddingObject:allColors];
-        UIColor* movedColor = [allColors firstObject];
-        allColors = [allColors subarrayWithRange:NSMakeRange(1, maxColors*2 - 1)];
-        allColors = [allColors arrayByAddingObject:movedColor];
-    }
-    
-    NSArray* adjustedColors = @[];
-    for (NSArray* colors in steppedColors) {
-        adjustedColors = [adjustedColors arrayByAddingObject:colors];
-    }
-    
-    NSArray* keyTimes = @[];
-    for (int i=0; i<maxColors*2; i++) {
-        keyTimes = [keyTimes arrayByAddingObject:[NSNumber numberWithFloat:(i/(maxColors*2))]];
-    }
-    
-    adjustedColors = [adjustedColors arrayByAddingObject:[adjustedColors firstObject]];
-    keyTimes = [keyTimes arrayByAddingObject:@(1.0)];
-    
-    adjustedColors = [adjustedColors map:^id(id obj, NSUInteger index) {
-        return [obj reversedArray];
-    }];
-
-    CAKeyframeAnimation *colorsAnimation = [CAKeyframeAnimation animationWithKeyPath:@"colors"];
-    colorsAnimation.values = adjustedColors;
-    colorsAnimation.keyTimes = keyTimes;
-    colorsAnimation.calculationMode = kCAAnimationLinear;
-    colorsAnimation.removedOnCompletion = NO;
-    colorsAnimation.duration = 1.5f;
-    colorsAnimation.repeatCount = HUGE_VALF; // repeat forever
-    [cloudGradientLayer addAnimation:colorsAnimation forKey:@"colorAnimation"];
-    
-    cloudGradientLayer.colors = [adjustedColors firstObject];
+    cloudGradientLayer.colors = allColors;
 
     return cloudGradientLayer;
 }
