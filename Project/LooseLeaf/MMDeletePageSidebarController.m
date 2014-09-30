09783c01946287b38600620e36b74c6949b28732
Adam Wulf <adam.wulf@gmail.com>
2014-09-30 15:01:23 -0500

    crumple texture for trash sidebar #791

--- Project/LooseLeaf/MMDeletePageSidebarController.m
+++ Project/LooseLeaf/MMDeletePageSidebarController.m
@@ -55,29 +55,29 @@
         
         // default fill w/o stripes
         UIBezierPath* fillPath = [UIBezierPath bezierPathWithArcCenter:center radius:radius - kBorderSpacing - kBorderWidth startAngle:0 endAngle:2*M_PI clockwise:YES];
-        CALayer* trashBackground = [self giveLayerDefaultProperties:[CALayer layer]];
-        trashBackground.backgroundColor = stripeColorBack.CGColor;
+        UIImageView* trashBackground = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"crumple.jpg"]];
+        trashBackground.alpha = 0.4;
         CAShapeLayer* trashBackgroundMask = [CAShapeLayer layer];
         trashBackgroundMask.backgroundColor = [UIColor whiteColor].CGColor;
         trashBackgroundMask.path = fillPath.CGPath;
-        trashBackground.mask = trashBackgroundMask;
+        trashBackground.layer.mask = trashBackgroundMask;
 
-        // now add the stripes to the fill
-        UIBezierPath* stripePath = [UIBezierPath bezierPath];
-        for(int i=0;i<ceil(frame.size.height / kStripeHeight);i++){
-            [stripePath appendPath:[UIBezierPath bezierPathWithRect:CGRectMake(0, 2*i*kStripeHeight, 2*frame.size.width, kStripeHeight)]];
-        }
-        [stripePath applyTransform:CGAffineTransformMakeTranslation(0, -10*kStripeHeight)];
-        [stripePath applyTransform:CGAffineTransformMakeTranslation(-frame.size.width/2, -frame.size.height/2)];
-        [stripePath applyTransform:CGAffineTransformMakeRotation(-20.0 * M_PI / 180.0)];
-        [stripePath applyTransform:CGAffineTransformMakeTranslation(frame.size.width/2, frame.size.height/2)];
-        CALayer* trashStripes = [self giveLayerDefaultProperties:[CALayer layer]];
-        trashStripes.backgroundColor = stripeColorFore.CGColor;
-        CAShapeLayer* trashStripesMask = [CAShapeLayer layer];
-        trashStripesMask.backgroundColor = [UIColor whiteColor].CGColor;
-        trashStripesMask.path = stripePath.CGPath;
-        trashStripes.mask = trashStripesMask;
-        [trashBackground addSublayer:trashStripes];
+//        // now add the stripes to the fill
+//        UIBezierPath* stripePath = [UIBezierPath bezierPath];
+//        for(int i=0;i<ceil(frame.size.height / kStripeHeight);i++){
+//            [stripePath appendPath:[UIBezierPath bezierPathWithRect:CGRectMake(0, 2*i*kStripeHeight, 2*frame.size.width, kStripeHeight)]];
+//        }
+//        [stripePath applyTransform:CGAffineTransformMakeTranslation(0, -10*kStripeHeight)];
+//        [stripePath applyTransform:CGAffineTransformMakeTranslation(-frame.size.width/2, -frame.size.height/2)];
+//        [stripePath applyTransform:CGAffineTransformMakeRotation(-20.0 * M_PI / 180.0)];
+//        [stripePath applyTransform:CGAffineTransformMakeTranslation(frame.size.width/2, frame.size.height/2)];
+//        CALayer* trashStripes = [self giveLayerDefaultProperties:[CALayer layer]];
+//        trashStripes.backgroundColor = stripeColorFore.CGColor;
+//        CAShapeLayer* trashStripesMask = [CAShapeLayer layer];
+//        trashStripesMask.backgroundColor = [UIColor whiteColor].CGColor;
+//        trashStripesMask.path = stripePath.CGPath;
+//        trashStripes.mask = trashStripesMask;
+//        [trashBackground addSublayer:trashStripes];
         
         
 
@@ -89,7 +89,8 @@
         
         deleteSidebarBackground.layer.backgroundColor = [UIColor clearColor].CGColor;
         [deleteSidebarBackground.layer addSublayer:rightBorder];
-        [deleteSidebarBackground.layer addSublayer:trashBackground];
+//        [deleteSidebarBackground.layer addSublayer:trashBackground];
+        [deleteSidebarBackground addSubview:trashBackground];
         
     }
     return self;
