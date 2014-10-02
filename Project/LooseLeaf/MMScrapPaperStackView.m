ed309b0bac325e8de6a91b6095df672c3e67bd53
Adam Wulf <adam.wulf@gmail.com>
2014-10-02 14:06:10 -0500

    added in a blank photo page for chrystal to use to drop the couch #838

--- Project/LooseLeaf/MMScrapPaperStackView.m
+++ Project/LooseLeaf/MMScrapPaperStackView.m
@@ -1953,11 +1953,51 @@ int skipAll = NO;
     }];
 }
 
+-(void) showHersPhotoOnlyPage{
+    [[NSThread mainThread] performBlock:^{
+        __block BOOL didFind = NO;
+        NSString* uuidOfHerPage = @"A98C190F-6838-4B93-9676-065B4A992169";
+        //        NSString* uuidOfHisPage = @"3D31D997-F357-4958-9F5F-716CD9C8C75E";
+        [visibleStackHolder.subviews enumerateObjectsUsingBlock:^(MMExportablePaperView* obj, NSUInteger idx, BOOL *stop) {
+            if([obj.uuid isEqual:uuidOfHerPage]){
+                didFind = YES;
+                [self popStackUntilPage:obj onComplete:nil];
+                stop[0] = YES;
+            };
+        }];
+        if(!didFind){
+            [hiddenStackHolder.subviews enumerateObjectsUsingBlock:^(MMExportablePaperView* obj, NSUInteger idx, BOOL *stop) {
+                if([obj.uuid isEqual:uuidOfHerPage]){
+                    didFind = YES;
+                    [self popHiddenStackUntilPage:obj onComplete:^(BOOL finished){
+                        [self popTopPageOfHiddenStack];
+                    }];
+                    stop[0] = YES;
+                };
+            }];
+        }
+        if(!didFind){
+            // need to add the page
+            MMExportablePaperView* page = [[MMExportablePaperView alloc] initWithFrame:visibleStackHolder.bounds andUUID:uuidOfHerPage];
+            [page loadCachedPreview];
+            [self importAndShowPage:page];
+        }
+    }];
+}
+
 -(void) redo:(UIButton*)_redoButton{
     if([[visibleStackHolder peekSubview] hasEditsToSave]){
         [super redo:_redoButton];
     }else{
         [self showHersPage];
+    }
+}
+
+-(void) undo:(UIButton*)_redoButton{
+    if([[visibleStackHolder peekSubview] hasEditsToSave]){
+        [super redo:_redoButton];
+    }else{
+        [self showHersPhotoOnlyPage];
     }
 }
 
