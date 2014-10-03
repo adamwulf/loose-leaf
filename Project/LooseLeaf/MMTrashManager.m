636b2a40b67692844b9cd26ab94bf6e30a15c4ff
Adam Wulf <adam.wulf@gmail.com>
2014-10-03 15:47:09 -0500

    trashmanager code cleanup and comments #863

--- Project/LooseLeaf/MMTrashManager.m
+++ Project/LooseLeaf/MMTrashManager.m
@@ -50,92 +50,10 @@ static MMTrashManager* _instance = nil;
 }
 
 
-#pragma mark - Delete Methods
+#pragma mark - Public Methods
 
 -(void) deleteScrap:(NSString*)scrapUUID inPage:(MMUndoablePaperView*)page{
-    //
-    // first check the bezel to see if the scrap exists outside the page
-    if([page.delegate.bezelContainerView containsScrapUUID:scrapUUID]){
-        NSLog(@"scrap %@ is in bezel, can't delete assets", scrapUUID);
-        return;
-    }
-
-    MMUndoablePaperView* undoablePage = nil;
-    if([page isKindOfClass:[MMUndoablePaperView class]]){
-        undoablePage = (MMUndoablePaperView*)page;
-    }
-    
-    dispatch_semaphore_t sema1 = dispatch_semaphore_create(0);
-
-    // first, we need to check if we're even eligible to
-    // delete the scrap or not.
-    //
-    // if the scrap is being held in the undo/redo manager
-    // then we need to keep the scraps assets on disk.
-    // otherwise we can delete them.
-    BOOL(^checkScrapExistsInUndoRedoManager)() = ^{
-        __block BOOL existsInUndoRedoManager = NO;
-        dispatch_async([page serialBackgroundQueue], ^{
-            BOOL needsLoad = ![undoablePage.undoRedoManager isLoaded];
-            if(needsLoad){
-                [undoablePage.undoRedoManager loadFrom:[undoablePage undoStatePath]];
-            }
-            existsInUndoRedoManager = [undoablePage.undoRedoManager containsItemForScrapUUID:scrapUUID];
-            if(needsLoad){
-                [undoablePage.undoRedoManager unloadState];
-            }
-            dispatch_semaphore_signal(sema1);
-        });
-        dispatch_semaphore_wait(sema1, DISPATCH_TIME_FOREVER);
-        return existsInUndoRedoManager;
-    };
-    
-    
-    // we've been told to delete a scrap from disk.
-    // so do this on our low priority background queue
-    dispatch_async([self trashManagerQueue], ^{
-        if(undoablePage && checkScrapExistsInUndoRedoManager()){
-            // the scrap exists in the page's undo manager,
-            // so don't bother deleting it
-            NSLog(@"TrashManager found scrap in page's undo state. keeping files.");
-            return;
-        }
-        
-        // delete from the page's scrapsOnPaperState
-        void(^removeFromScrapsOnPaperState)() = ^{
-            [page.scrapsOnPaperState removeScrapWithUUID:scrapUUID];
-            [[page.scrapsOnPaperState immutableStateForPath:page.scrapIDsPath] saveStateToDiskBlocking];
-        };
-        
-        // now the scrap is off disk, so remove it from the page's state too
-        if([page.scrapsOnPaperState isStateLoaded]){
-            removeFromScrapsOnPaperState();
-        }else{
-            [page performBlockForUnloadedScrapStateSynchronously:removeFromScrapsOnPaperState];
-        }
-
-        // now that the scrap is out of the page's state, then
-        // we can delete it off disk too
-        NSString* documentsPath = [NSFileManager documentsPath];
-        NSString* pagesPath = [[documentsPath stringByAppendingPathComponent:@"Pages"] stringByAppendingPathComponent:page.uuid];
-        NSString* scrapPath = [[pagesPath stringByAppendingPathComponent:@"Scraps"] stringByAppendingPathComponent:scrapUUID];
-        BOOL isDirectory = NO;
-        if([[NSFileManager defaultManager] fileExistsAtPath:scrapPath isDirectory:&isDirectory]){
-            if(isDirectory){
-                NSError* err = nil;
-                if([[NSFileManager defaultManager] removeItemAtPath:scrapPath error:&err]){
-                    NSLog(@"deleted1 %@", scrapPath);
-                }
-                if(err){
-                    NSLog(@"error deleting %@: %@", scrapPath, err);
-                }
-            }else{
-//                NSLog(@"found path, but it isn't a directory");
-            }
-        }else{
-//            NSLog(@"path to delete doesn't exist %@", scrapPath);
-        }
-    });
+    [self deleteScrap:scrapUUID inPage:page shouldRespectUndoManager:YES];
 }
 
 
@@ -181,5 +99,120 @@ static MMTrashManager* _instance = nil;
         });
     });
 }
+
+
+
+#pragma mark - Helper Methods
+
+//
+// @param scrapUUID attempt to delete the assets of this scrap in the input page, but only
+//        if the scrap is unused in the bezel and the page's own undo manager (optionally)
+// @param page the page who's scrap should be deleted
+// @param respectUndoManager YES if we should keep scraps in the page's undoManager, NO if we should ignore the undo manager
+//        (for instance, when deleting the page)
+-(void) deleteScrap:(NSString*)scrapUUID inPage:(MMUndoablePaperView*)page shouldRespectUndoManager:(BOOL)respectUndoManager{
+
+    if(!page || !scrapUUID){
+        // sanity
+        NSLog(@"can't delete scrap %@ from page %@", scrapUUID, page);
+        return;
+    }
+
+    //
+    // Step 1: check the bezel
+    //
+    // first check the bezel to see if the scrap exists outside the page
+    if([page.delegate.bezelContainerView containsScrapUUID:scrapUUID]){
+        NSLog(@"scrap %@ is in bezel, can't delete assets", scrapUUID);
+        return;
+    }
+    
+    // first, we need to check if we're even eligible to
+    // delete the scrap or not.
+    //
+    // if the scrap is being held in the undo/redo manager
+    // then we need to keep the scraps assets on disk.
+    // otherwise we can delete them.
+    BOOL(^checkScrapExistsInUndoRedoManager)() = ^{
+        dispatch_semaphore_t sema1 = dispatch_semaphore_create(0);
+        __block BOOL existsInUndoRedoManager = NO;
+        dispatch_async([page serialBackgroundQueue], ^{
+            BOOL needsLoad = ![page.undoRedoManager isLoaded];
+            if(needsLoad){
+                [page.undoRedoManager loadFrom:[page undoStatePath]];
+            }
+            existsInUndoRedoManager = [page.undoRedoManager containsItemForScrapUUID:scrapUUID];
+            if(needsLoad){
+                [page.undoRedoManager unloadState];
+            }
+            dispatch_semaphore_signal(sema1);
+        });
+        dispatch_semaphore_wait(sema1, DISPATCH_TIME_FOREVER);
+        return existsInUndoRedoManager;
+    };
+    
+    
+    // we've been told to delete a scrap from disk.
+    // so do this on our low priority background queue
+    dispatch_async([self trashManagerQueue], ^{
+        //
+        // Step 2: check the undo manager for the page
+        //         (optionally)
+        if(respectUndoManager){
+            // only check the undo manager if we were asked to.
+            // we might ignore it if we're trying to delete
+            // the page as well
+            if(page && checkScrapExistsInUndoRedoManager()){
+                // the scrap exists in the page's undo manager,
+                // so don't bother deleting it
+                NSLog(@"TrashManager found scrap in page's undo state. keeping files.");
+                return;
+            }
+        }
+        
+        //
+        // if we made it this far, then the scrap is not in the page's
+        // undo manager, and it's not in the bezel, so it's safe to delete
+        //
+        // Step 3: delete from the page's state
+        // now the scrap is off disk, so remove it from the page's state too
+        // delete from the page's scrapsOnPaperState
+        void(^removeFromScrapsOnPaperState)() = ^{
+            [page.scrapsOnPaperState removeScrapWithUUID:scrapUUID];
+            [[page.scrapsOnPaperState immutableStateForPath:page.scrapIDsPath] saveStateToDiskBlocking];
+        };
+        if([page.scrapsOnPaperState isStateLoaded]){
+            removeFromScrapsOnPaperState();
+        }else{
+            [page performBlockForUnloadedScrapStateSynchronously:removeFromScrapsOnPaperState];
+        }
+        
+        //
+        // Step 4: delete the assets off disk
+        // now that the scrap is out of the page's state, then
+        // we can delete it off disk too
+        NSString* documentsPath = [NSFileManager documentsPath];
+        NSString* pagesPath = [[documentsPath stringByAppendingPathComponent:@"Pages"] stringByAppendingPathComponent:page.uuid];
+        NSString* scrapPath = [[pagesPath stringByAppendingPathComponent:@"Scraps"] stringByAppendingPathComponent:scrapUUID];
+        BOOL isDirectory = NO;
+        if([[NSFileManager defaultManager] fileExistsAtPath:scrapPath isDirectory:&isDirectory]){
+            if(isDirectory){
+                NSError* err = nil;
+                if([[NSFileManager defaultManager] removeItemAtPath:scrapPath error:&err]){
+                    NSLog(@"deleted1 %@", scrapPath);
+                }
+                if(err){
+                    NSLog(@"error deleting %@: %@", scrapPath, err);
+                }
+            }else{
+                //                NSLog(@"found path, but it isn't a directory");
+            }
+        }else{
+            //            NSLog(@"path to delete doesn't exist %@", scrapPath);
+        }
+    });
+}
+
+
 
 @end
