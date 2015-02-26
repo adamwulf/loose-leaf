0a4e7df09de4b0d20f2f9ce24479dd1f4eb942d1
Adam Wulf <adam.wulf@gmail.com>
2015-02-25 18:24:43 -0600

    background saves and loads from disk #1169

--- Project/LooseLeaf/MMBackgroundedPaperView.m
+++ Project/LooseLeaf/MMBackgroundedPaperView.m
@@ -8,9 +8,11 @@
 
 #import "MMBackgroundedPaperView.h"
 #import "MMEditablePaperViewSubclass.h"
+#import "NSThread+BlockAdditions.h"
 
 @implementation MMBackgroundedPaperView{
     UIImageView* paperBackgroundView;
+    NSString* backgroundTexturePath;
 }
 
 -(BOOL) isVert:(UIImage*)img{
@@ -30,6 +32,10 @@
 }
 
 -(void) setPageBackgroundTexture:(UIImage*)img{
+    [self setPageBackgroundTexture:img andSaveToDisk:YES];
+}
+
+-(void) setPageBackgroundTexture:(UIImage*)img andSaveToDisk:(BOOL)saveToDisk{
     CheckMainThread;
     if(img.size.width > img.size.height){
         // rotate
@@ -47,8 +53,48 @@
         }else{
             [self.contentView insertSubview:paperBackgroundView belowSubview:scrapsOnPaperState.scrapContainerView];
         }
+        if(drawableView && !drawableView.hidden){
+            paperBackgroundView.hidden = NO;
+        }else{
+            paperBackgroundView.hidden = YES;
+        }
     }
     paperBackgroundView.image = img;
+    
+    if(saveToDisk){
+        dispatch_async([MMEditablePaperView importThumbnailQueue], ^{
+            [UIImagePNGRepresentation(img) writeToFile:[self backgroundTexturePath] atomically:YES];
+        });
+    }
+}
+
+#pragma mark - Loading and Unloading State
+
+-(void) loadStateAsynchronously:(BOOL)async withSize:(CGSize)pagePtSize andScale:(CGFloat)scale andContext:(JotGLContext *)context{
+    [super loadStateAsynchronously:async withSize:pagePtSize andScale:scale andContext:context];
+    
+    void (^loadPageBackgroundFromDisk)() = ^{
+        UIImage* img = [UIImage imageWithContentsOfFile:[self backgroundTexturePath]];
+        if(img){
+            [[NSThread mainThread] performBlock:^{
+                [self setPageBackgroundTexture:img andSaveToDisk:NO];
+            }];
+        }
+    };
+    
+    if(async){
+        dispatch_async([MMEditablePaperView importThumbnailQueue], loadPageBackgroundFromDisk);
+    }else{
+        // we're already on the correct thread, so just run it now
+        loadPageBackgroundFromDisk();
+    }
+
+}
+
+-(void) unloadState{
+    [super unloadState];
+    [paperBackgroundView removeFromSuperview];
+    paperBackgroundView = nil;
 }
 
 #pragma mark - Thumbnail Generation
@@ -61,6 +107,15 @@
 //        CGContextDrawImage(context, CGRectMake(0, 0, thumbSize.width, thumbSize.height), paperBackgroundView.image.CGImage);
         UIGraphicsPopContext();
     }
+}
+
+#pragma mark - Paths
+
+-(NSString*) backgroundTexturePath{
+    if(!backgroundTexturePath){
+        backgroundTexturePath = [[[self pagesPath] stringByAppendingPathComponent:@"backgroundTexture"] stringByAppendingPathExtension:@"png"];
+    }
+    return backgroundTexturePath;
 }
 
 
