2dd66c3b4631dcfa27767d4718a27c8e8daf9876
Adam Wulf <adam.wulf@gmail.com>
2015-08-08 00:32:01 -0500

    tutorial button doesn't show up above the import sidebar #1502

--- Project/LooseLeaf/MMTutorialStackView.m
+++ Project/LooseLeaf/MMTutorialStackView.m
@@ -28,7 +28,7 @@
         }];
         helpButton.delegate = self;
         [helpButton addTarget:self action:@selector(tutorialButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:helpButton];
+        [self insertSubview:helpButton aboveSubview:(__bridge UIView *)(buttons[numberOfButtons-1].button)];
         buttons[numberOfButtons].button = (__bridge void *)(helpButton);
         buttons[numberOfButtons].originalRect = helpButton.frame;
         numberOfButtons++;
