f6fa08fec0f7e43ad5613e6ed1ffd4d7e8b65d9d
Adam Wulf <adam.wulf@gmail.com>
2016-05-19 23:29:30 -0500

    Cleaned up some opengl issues w/ JotView. #1613

--- Project/LooseLeaf/MMGenericBackgroundView.m
+++ Project/LooseLeaf/MMGenericBackgroundView.m
@@ -39,60 +39,62 @@
 // refactor this in the future so that scraps contain a generic background
 // instead of a specific subclass of background. same for pages.
 -(MMScrapBackgroundView*) stampBackgroundFor:(MMScrapViewState*)targetScrapState{
-    // Find the relative rotation of the target scrap vs us
-    CGFloat orgRot = [self.delegate contextRotationForGenericBackground:self];
-    CGFloat newRot = targetScrapState.delegate.rotation;
-    CGFloat rotDiff = orgRot - newRot;
-    
-    // also calculate its center vs our center
-    CGPoint convertedC = [targetScrapState.contentView convertPoint:[self.delegate currentCenterOfBackgroundForGenericBackground:self] fromView:[self.delegate contextViewForGenericBackground:self]];
-    
-    CGSize backingImageSize = _backingImage.size;
-    CGSize targetImageSize = targetScrapState.originalSize;
-    CGFloat targetRotation = self.backgroundRotation + rotDiff;
-    CGFloat targetScale = self.backgroundScale;
-    
-    // our target image size may not be on an exact pixel boundary
-    // since its based off of the target scrap's bezier path's
-    // bounding box. Let's round up to the nearest point.
-    double widthInt = 0;
-    CGFloat widthFrac = modf(targetImageSize.width, &widthInt);
-    targetImageSize.width += (1 - widthFrac);
-
-    double heightInt = 0;
-    CGFloat heightFrac = modf(targetImageSize.height, &heightInt);
-    targetImageSize.height += (1 - heightFrac);
-    
-    UIGraphicsBeginImageContextWithOptions(targetImageSize, NO, [[UIScreen mainScreen] scale]);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    [[UIColor whiteColor] setFill];
-    CGContextFillRect(context, CGRectMake(0, 0, targetImageSize.width, targetImageSize.height));
-
-    // translate into the center of the context
-    CGContextTranslateCTM(context, convertedC.x, convertedC.y);
-    
-    // No idea currently why i need this offset. There must be an offset somewhere
-    // for scraps that I'm not remembering, but I'm not finding it.
-    CGContextTranslateCTM(context, -4, -4);
-    
-    // rotate to match our target scrap orientation
-    CGContextRotateCTM(context, targetRotation);
-
-    // match our target scrap scale
-    CGContextScaleCTM(context, targetScale, targetScale);
-    
-    // now draw the image centered at our current point
-    [self.backingImage drawInRect:CGRectMake(-backingImageSize.width/2, -backingImageSize.height/2, backingImageSize.width, backingImageSize.height)];
-    
-    UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    MMScrapBackgroundView* backgroundView = [[MMScrapBackgroundView alloc] initWithImage:image forScrapState:targetScrapState];
-    backgroundView.backgroundScale = 1.0;
-    backgroundView.backgroundRotation = 0;
-    backgroundView.backgroundOffset = CGPointZero;
-    
-    return backgroundView;
+    @autoreleasepool {
+        // Find the relative rotation of the target scrap vs us
+        CGFloat orgRot = [self.delegate contextRotationForGenericBackground:self];
+        CGFloat newRot = targetScrapState.delegate.rotation;
+        CGFloat rotDiff = orgRot - newRot;
+        
+        // also calculate its center vs our center
+        CGPoint convertedC = [targetScrapState.contentView convertPoint:[self.delegate currentCenterOfBackgroundForGenericBackground:self] fromView:[self.delegate contextViewForGenericBackground:self]];
+        
+        CGSize backingImageSize = _backingImage.size;
+        CGSize targetImageSize = targetScrapState.originalSize;
+        CGFloat targetRotation = self.backgroundRotation + rotDiff;
+        CGFloat targetScale = self.backgroundScale;
+        
+        // our target image size may not be on an exact pixel boundary
+        // since its based off of the target scrap's bezier path's
+        // bounding box. Let's round up to the nearest point.
+        double widthInt = 0;
+        CGFloat widthFrac = modf(targetImageSize.width, &widthInt);
+        targetImageSize.width += (1 - widthFrac);
+        
+        double heightInt = 0;
+        CGFloat heightFrac = modf(targetImageSize.height, &heightInt);
+        targetImageSize.height += (1 - heightFrac);
+        
+        UIGraphicsBeginImageContextWithOptions(targetImageSize, NO, [[UIScreen mainScreen] scale]);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        [[UIColor whiteColor] setFill];
+        CGContextFillRect(context, CGRectMake(0, 0, targetImageSize.width, targetImageSize.height));
+        
+        // translate into the center of the context
+        CGContextTranslateCTM(context, convertedC.x, convertedC.y);
+        
+        // No idea currently why i need this offset. There must be an offset somewhere
+        // for scraps that I'm not remembering, but I'm not finding it.
+        CGContextTranslateCTM(context, -4, -4);
+        
+        // rotate to match our target scrap orientation
+        CGContextRotateCTM(context, targetRotation);
+        
+        // match our target scrap scale
+        CGContextScaleCTM(context, targetScale, targetScale);
+        
+        // now draw the image centered at our current point
+        [self.backingImage drawInRect:CGRectMake(-backingImageSize.width/2, -backingImageSize.height/2, backingImageSize.width, backingImageSize.height)];
+        
+        UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        MMScrapBackgroundView* backgroundView = [[MMScrapBackgroundView alloc] initWithImage:image forScrapState:targetScrapState];
+        backgroundView.backgroundScale = 1.0;
+        backgroundView.backgroundRotation = 0;
+        backgroundView.backgroundOffset = CGPointZero;
+        
+        return backgroundView;
+    }
 }
 
 
