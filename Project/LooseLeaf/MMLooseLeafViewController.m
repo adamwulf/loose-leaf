d454975a47ef3afe107bc0d8ccc4435ff80baaa6
Adam Wulf <adam.wulf@gmail.com>
2016-10-28 02:41:45 -0500

    Turned off release notes ui for debug

--- Project/LooseLeaf/MMLooseLeafViewController.m
+++ Project/LooseLeaf/MMLooseLeafViewController.m
@@ -720,9 +720,9 @@
         return;
     }
 
-#ifdef DEBUG
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kLastOpenedVersion];
-#endif
+    //#ifdef DEBUG
+    //    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kLastOpenedVersion];
+    //#endif
 
     if (version && ![[[NSUserDefaults standardUserDefaults] stringForKey:kLastOpenedVersion] isEqualToString:version]) {
         NSURL* releaseNotesFile = [[NSBundle mainBundle] URLForResource:[NSString stringWithFormat:@"ReleaseNotes-%@", version] withExtension:@"md"];
