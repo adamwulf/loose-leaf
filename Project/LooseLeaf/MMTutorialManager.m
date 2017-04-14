2ad3d34ed48ee17efaf0835860de0a51ebe2daa0
Adam Wulf <adam.wulf@gmail.com>
2017-04-14 00:19:12 -0500

    First draft of translations. #1699

--- Project/LooseLeaf/MMTutorialManager.m
+++ Project/LooseLeaf/MMTutorialManager.m
@@ -106,32 +106,32 @@ static MMTutorialManager* _instance = nil;
     return @[
         @{
             @"id": @"app-intro-pen",
-            @"title": @"Draw and Erase",
+            @"title": NSLocalizedString(@"Draw and Erase", @"Draw and Erase"),
             @"video": @"draw-tools.mp4"
         },
         @{
             @"id": @"app-intro-pinch",
-            @"title": @"Pinch to See Your Pages",
+            @"title": NSLocalizedString(@"Pinch to See Your Pages", @"Pinch to See Your Pages"),
             @"video": @"pinch-to-list.mp4"
         },
         @{
             @"id": @"app-intro-import-scissor",
-            @"title": @"Import and Crop Your Photos",
+            @"title": NSLocalizedString(@"Import and Crop Your Photos", @"Import and Crop Your Photos"),
             @"video": @"import-and-scissor.mov"
         },
         @{
             @"id": @"app-intro-scrap-sidebar",
-            @"title": @"Move Scraps Between Pages",
+            @"title": NSLocalizedString(@"Move Scraps Between Pages", @"Move Scraps Between Pages"),
             @"video": @"scrap-to-sidebar.mov"
         },
         @{
             @"id": @"app-intro-scrap-reorder",
-            @"title": @"Shake a Scrap to Reorder",
+            @"title": NSLocalizedString(@"Shake a Scrap to Reorder", @"Shake a Scrap to Reorder"),
             @"video": @"shake-to-reorder.mov"
         },
         @{
             @"id": @"app-intro-export",
-            @"title": @"Export Your Pages",
+            @"title": NSLocalizedString(@"Export Your Pages", @"Export Your Pages"),
             @"video": @"export-to-facebook.mov"
         }
     ];
@@ -141,27 +141,27 @@ static MMTutorialManager* _instance = nil;
     return @[
         @{
             @"id": @"collapse-stack",
-            @"title": @"Move Between Pages",
+            @"title": NSLocalizedString(@"Move Between Pages", @"Move Between Pages"),
             @"video": @"collapse-stack.mp4"
         },
         @{
             @"id": @"reorder-stack",
-            @"title": @"Organize Your pages",
+            @"title": NSLocalizedString(@"Organize Your pages", @"Organize Your pages"),
             @"video": @"reorder-stack.mp4"
         },
         @{
             @"id": @"delete-stack",
-            @"title": @"Delete Your Notes",
+            @"title": NSLocalizedString(@"Delete Your Notes", @"Delete Your Notes"),
             @"video": @"delete-stack.mp4"
         },
         @{
             @"id": @"export-stack",
-            @"title": @"Export Your Notes",
+            @"title": NSLocalizedString(@"Export Your Notes", @"Export Your Notes"),
             @"video": @"export-stack.mp4"
         },
         @{
             @"id": @"import-stack",
-            @"title": @"Import Full PDFs",
+            @"title": NSLocalizedString(@"Import Full PDFs", @"Import Full PDFs"),
             @"video": @"import-stack.mp4"
         }
     ];
@@ -171,22 +171,22 @@ static MMTutorialManager* _instance = nil;
     return @[
         @{
             @"id": @"list-view-reorder-pages",
-            @"title": @"Organize Your Pages",
+            @"title": NSLocalizedString(@"Organize Your Pages", @"Organize Your Pages"),
             @"video": @"list-view-reorder-pages.mov"
         },
         @{
             @"id": @"page-sidebar",
-            @"title": @"Organize Your Pages",
+            @"title": NSLocalizedString(@"Organize Your Pages", @"Organize Your Pages"),
             @"video": @"page-sidebar.mp4"
         },
         @{
             @"id": @"list-view-delete-page",
-            @"title": @"Delete a Page",
+            @"title": NSLocalizedString(@"Delete a Page", @"Delete a Page"),
             @"video": @"list-view-delete-page.mov"
         },
         @{
             @"id": @"list-view-clone-page",
-            @"title": @"Duplicate a Page",
+            @"title": NSLocalizedString(@"Duplicate a Page", @"Duplicate a Page"),
             @"video": @"list-view-clone-page.mp4"
         }
     ];
@@ -196,22 +196,22 @@ static MMTutorialManager* _instance = nil;
     return @[
         @{
             @"id": @"share-social",
-            @"title": @"Share to Your Social Networks",
+            @"title": NSLocalizedString(@"Share to Your Social Networks", @"Share to Your Social Networks"),
             @"video": @"share-tutorials-social.png"
         },
         @{
             @"id": @"share-contact",
-            @"title": @"Share Your Pages with Your Contacts",
+            @"title": NSLocalizedString(@"Share Your Pages with Your Contacts", @"Share Your Pages with Your Contacts"),
             @"video": @"share-tutorials-contacts.png"
         },
         @{
             @"id": @"share-export",
-            @"title": @"Export Your Pages to Anywhere",
+            @"title": NSLocalizedString(@"Export Your Pages to Anywhere", @"Export Your Pages to Anywhere"),
             @"video": @"share-tutorials-export.png"
         },
         @{
             @"id": @"share-cloudkit",
-            @"title": @"Send a page to a friend with CloudKit",
+            @"title": NSLocalizedString(@"Send a page to a friend with CloudKit", @"Send a page to a friend with CloudKit"),
             @"video": @"share-tutorials-cloudkit.png"
         }
     ];
